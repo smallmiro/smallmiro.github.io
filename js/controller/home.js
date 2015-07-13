8c5ad35521364394a392956b8a4da647cf6afb89
오준환 <junhwan.oh@nhnent.com>
2015-07-13 18:34:44 +0900

    CROS설정 추

--- js/controller/home.js
+++ js/controller/home.js
@@ -1,25 +1,21 @@
-var baseUrl = "http://ec2-54-152-124-0.compute-1.amazonaws.com:8080";
+var baseUrl = "http://LEGO-ELB-61416480.us-east-1.elb.amazonaws.com";
 var mainHomeApp = angular.module('topApp', []);
+
 mainHomeApp.factory('product', ['$http',  function($http) {
   var orders = [];
   return {
-    getPrice: function(priceCode) {
-      var priceUrl = "/gonghome/price/" + priceCode + "?callback=JSON_CALLBACK";
-      return $http.jsonp(baseUrl + priceUrl)
+    getPrice: function(priceCode, callback) {
+      var priceUrl = "/gonghome/price/" + priceCode + "";
+      $http.get(baseUrl + priceUrl)
           .success(function(data){
-              console.log(data);
+              callback(data);
           });
     },
-    getProduct: function(inputUrl) {
-      var callurl = baseUrl + "/gonghome/meta?callback=JSON_CALLBACK";
-      $http({
-        method: 'JSONP',
-        url: callurl,
-        data  : { url : inputUrl }
-      }).success(function(data, status , header, config){
-           console.log(data);
-      }).error(function(data, status , header, config){
-           console.log('error');
+    getProduct: function(inputUrl, callback) {
+      var callurl = baseUrl + "/gonghome/meta";
+      $http.post(callurl ,  { url : inputUrl }, headers: {'x-requested-with': ''}
+      ).success(function(data, status , header, config){
+           callback(data);
       });
     }
   };
@@ -27,15 +23,14 @@ mainHomeApp.factory('product', ['$http',  function($http) {
 
 mainHomeApp.controller('legoController',  function($scope, $http, product) {
   $scope.addAlert = function() {
-    product.getPrice("10218").then(function(response) {
-      console.log(response);
-    })
+    product.getPrice("10218", function(data){
+      console.log(data);
+    });
   };
 
-  $scope.getProduct = function(){
-    console.log("getProduct");
-    product.getPrice($scope.shopUrl).then(function(response) {
-      console.log(response);
-    })
+  $scope.getPrice = function(){
+    product.getProduct($scope.shopUrl, function(data){
+      console.log(data);
+    });
   };
 });
