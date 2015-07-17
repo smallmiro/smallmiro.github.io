0b07ca72070f4f1a3599acda535b7153228eb504
오준환 <junhwan.oh@nhnent.com>
2015-07-17 17:37:26 +0900

    [오준환][NOBTS] 캐쉬 추가

--- js/controller/home.js
+++ js/controller/home.js
@@ -119,8 +119,11 @@ mainHomeApp.factory('product', ['$http',  function($http) {
 
 mainHomeApp.controller('legoController',  function($scope, $http, $window, product) {
   $scope.checkShow = function(price){
-    if(price.krw > 0 && price.availabilityMessage != 'Retired product') {
+    if(price.krw <= 0 && price.availabilityMessage != 'Retired product') {
       return true;
+    } else if(price.krw <= 0) {
+      return true;
+    }
     } else {
       return false;
     }
