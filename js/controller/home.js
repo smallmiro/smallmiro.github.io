a5949553c0a58596bc8c5df43d35d9abb0c72973
오준환 <junhwan.oh@nhnent.com>
2015-07-17 17:47:45 +0900

    [오준환][NOBTS] 캐쉬 추가

--- js/controller/home.js
+++ js/controller/home.js
@@ -6,11 +6,11 @@ mainHomeApp.filter('prodStatus', ['$filter', function($filter) {
   return function(price) {
     if (! price.availabilityMessage) return "success";
     var message = price.availabilityMessage;
-    if(message.indexOf("Out of stock") >=0 || message == '일시품절' || message.indexOf("Call to check") >=0  || price.krw <= 0){
+    if(message.indexOf("Out of stock") >=0 || message == '일시품절' || message.indexOf("Call to check") >=0  || price.krw <= 0 || message.indexOf("불가능") >=0){
       return "warning"
     } else if(message.indexOf('Sold Out') >=0 ) {
         return "danger"
-    } else if(message.indexOf('Coming Soon') >=0 ) {
+    } else if(message.indexOf('Coming Soon') >=0 || message.indexOf('출시예정') >=0) {
         return "info"
     } else if(message.indexOf('Retired product') >=0 ) {
         return "primary"
