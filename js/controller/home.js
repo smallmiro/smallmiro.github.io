6fbbf632c019496b8eaf8b9f1e3ee1278faa9834
오준환 <junhwan.oh@nhnent.com>
2015-07-23 10:53:40 +0900

    가운데 정렬  .

--- js/controller/home.js
+++ js/controller/home.js
@@ -13,7 +13,7 @@ mainHomeApp.filter('prodStatus', ['$filter', function($filter) {
     var message = price.availabilityMessage;
     if(price.saleStatus == "END") {
         return "danger"
-    } else if(message.indexOf("Out of stock") >=0 || message == '일시품절' || message.indexOf("Call to check") >=0  || price.krw <= 0 || message.indexOf("불가능") >=0){
+    } else if(message.indexOf("Out of stock") >=0 || message.indexOf("out of stock") >=0 || message == '일시품절' || message.indexOf("Call to check") >=0  || price.krw <= 0 || message.indexOf("불가능") >=0){
       return "warning"
     } else if(message.indexOf('Sold Out') >=0) {
         return "danger"
