f165aef22a23ecd4d0ebcdefc1d5b38ff3797d51
오준환 <junhwan.oh@nhnent.com>
2015-09-07 18:09:29 +0900

    광고 추가..

--- js/controller/home.js
+++ js/controller/home.js
@@ -157,7 +157,7 @@ mainHomeApp.factory('product', ['$http',  function($http) {
       });
     },
     getAd: function(callback, errorcallback) {
-      var callurl = "/data/ad.json?2015090702";
+      var callurl = "/data/ad.json?2015090703";
       $http.get(callurl)
           .success(function(data){
               callback(data);
